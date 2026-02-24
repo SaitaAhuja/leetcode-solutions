1314cf861b9c9d503f052d43463fb014c91a8c60
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-02-24 23:41:11 +0500

    Added LeetCode Problem: Fizz_Buzz.cpp

---
+++ FizzBuzz/Fizz_Buzz.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+
+        for(int i = 1; i <= n; i++) {
+
+            if(i % 3 == 0 && i % 5 == 0)
+                result.push_back("FizzBuzz");
+
+            else if(i % 3 == 0)
+                result.push_back("Fizz");
+
+            else if(i % 5 == 0)
+                result.push_back("Buzz");
+
+            else
+                result.push_back(to_string(i));
+        }
+
+        return result;
+    }
+};
+int main(){
+Solution sol;
+   int n;
+
+    cout << "Enter n:" << endl;
+    cin >> n;
+
+    vector<string> output = sol.fizzBuzz(n);
+
+    cout << "[ ";
+    for(int i = 0; i < output.size(); i++){  
+        cout << output[i];
+        if(i != output.size() - 1){
+            cout << ", ";
+        }
+    }
+    cout << " ]" << endl;
+
+    return 0;
+}
