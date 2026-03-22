7a847333ab4b577c4699f92223d965af1533d2f3
SaitaAhuja <160378533+SaitaAhuja@users.noreply.github.com>
2026-03-22 22:36:24 +0500

    Added Fibonacci_numbers.cpp

---
+++ Fibonacci Numbers/Fibonacci_numbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+   int fib(int n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    int a = 0, b = 1, c = 0;
+
+    for(int i = 2; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+};
+int main(){
+	int n;
+	cin>>n;
+	Solution sol;
+	cout<<sol.fib(n)<<endl;
+	return 0;
+}
